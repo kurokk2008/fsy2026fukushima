68fad09396608b60201fab4370bc51dda9cd3df9
kurokk2008 <42135677+kurokk2008@users.noreply.github.com>
2026-04-15 15:05:42 +0900

    Create program-data.js

---
+++ program-data.js
@@ -0,0 +1,45 @@
+window.PROGRAM_DATA = {
+  eventDays: [
+    {
+      key: "day1",
+      label: "1日目",
+      date: "2026-05-04",
+      title: "1日目プログラム",
+      items: [
+        { time: "12:00", title: "受付開始" },
+        { time: "13:00", title: "開会・オリエンテーション" },
+        { time: "15:00", title: "グループ活動" },
+        { time: "18:00", title: "夕食" },
+        { time: "20:00", title: "夜の集会" }
+      ]
+    },
+    {
+      key: "day2",
+      label: "2日目",
+      date: "2026-05-05",
+      title: "2日目プログラム",
+      items: [
+        { time: "07:00", title: "起床" },
+        { time: "08:00", title: "朝食" },
+        { time: "09:00", title: "朝の集合" },
+        { time: "10:00", title: "午前プログラム" },
+        { time: "12:00", title: "昼食" },
+        { time: "14:00", title: "午後プログラム" },
+        { time: "18:00", title: "夕食" }
+      ]
+    },
+    {
+      key: "day3",
+      label: "3日目",
+      date: "2026-05-06",
+      title: "3日目プログラム",
+      items: [
+        { time: "07:00", title: "起床" },
+        { time: "08:00", title: "朝食" },
+        { time: "09:00", title: "片付け・準備" },
+        { time: "10:00", title: "閉会" },
+        { time: "11:00", title: "解散" }
+      ]
+    }
+  ]
+};
